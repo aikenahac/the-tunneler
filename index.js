f631fd658f6f023d87a174d808bae8e2bce250c6
Gašper Dobrovoljc <gasper@yourflare.io>
2021-03-02 00:46:35 +0100

    ✨ Nicknames

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ require('dotenv').config();
 
 const TOKEN = process.env.TOKEN;
 
-console.log("TOKEN: " + TOKEN);
-
 console.log("Tunnel: " + config.tunnel);
 
 client.once('ready', () => {
@@ -16,54 +14,54 @@ client.once('ready', () => {
 });
 
 client.on('message', async message => {
-	try {
-		if (!config.tunnel.includes(message.channel.id) || message.webhookID) return;
+	if (!config.tunnel.includes(message.channel.id) || message.webhookID) return;
 
-		const channels = config.tunnel.filter(id => id !== message.channel.id)
+	const channels = config.tunnel.filter(id => id !== message.channel.id)
 
-		channels.forEach(async channelId => {
-			const channel = client.channels.cache.get(channelId);
+	channels.forEach(async channelId => {
+		const channel = client.channels.cache.get(channelId);
 
-			const webhooks = await channel.fetchWebhooks()
-			const myWebhooks = webhooks.filter(webhook => {
-                if (!webhook.owner) return;
-                console.log("Webhook owner: " + webhook.owner);
-                console.log("Client user: " + client.user);
-    
-                return webhook.owner.id === client.user.id && webhook.name === 'The Tunneler';
-            })
+		const webhooks = await channel.fetchWebhooks()
+		const myWebhooks = webhooks.filter(webhook => {
+			if (!webhook.owner) return;
 
-            let messageToSend;
-            let imagesArray = [];
-
-            if (message.attachments.size > 0) {
-                message.attachments.forEach(attachment => {
-                    const url = attachment.url;
-
-                    imagesArray.push(url);
-                })
-            }
-
-            if (imagesArray[0] === undefined) {
-                messageToSend = message.content;
-            } else if((message.content === null || message.content === '') && imagesArray[0] !== undefined) {
-                messageToSend = imagesArray;
-            } else {
-                messageToSend = message.content + "\n" + imagesArray;
-            }
-
-            console.log(messageToSend);
-
-			myWebhooks.first().send(messageToSend || '.', {
-				username: message.author.username,
-				avatarURL: message.author.avatarURL(),
-				embeds: message.embeds
-			})
-            console.log("Sent message!");
+			return webhook.owner.id === client.user.id && webhook.name === 'The Tunneler';
 		})
-	} catch (error) {
-		console.log(error)
-	}
+
+		let messageToSend;
+		const imagesArray = [];
+
+		if (message.attachments.size > 0) {
+			message.attachments.forEach(attachment => {
+				const url = attachment.url;
+
+				imagesArray.push(url);
+			})
+		}
+
+		let member;
+		try {
+			member = await channel.guild.members.fetch(message.author.id);
+		} catch (e) {
+			console.log('User is not in this guild');
+		}
+
+		if (imagesArray[0] === undefined) {
+			messageToSend = message.content;
+		} else if((message.content === null || message.content === '') && imagesArray[0] !== undefined) {
+			messageToSend = imagesArray;
+		} else {
+			messageToSend = message.content + "\n" + imagesArray;
+		}
+
+		console.log(`${message.author.username} > ${messageToSend}`);
+		
+		myWebhooks.first().send(messageToSend || 'No content provided', {
+			username: member ? member.nickname : message.author.username,
+			avatarURL: message.author.avatarURL(),
+			embeds: message.embeds,
+		})
+	})
 })
 
 client.login(TOKEN);
@@ -74,8 +72,6 @@ async function initWebhooks(channelId) {
 		const webhooks = await channel.fetchWebhooks()
 		const myWebhooks = webhooks.filter(webhook => {
             if (!webhook.owner) return;
-            console.log("Webhook owner: " + webhook.owner);
-            console.log("Client user: " + client.user);
 
             return webhook.owner.id === client.user.id && webhook.name === 'The Tunneler';
         })
